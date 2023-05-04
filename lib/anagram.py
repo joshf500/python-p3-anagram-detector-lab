57077d8d5ca2cdbe7d187d98f283b4d79197d358
Josh Feldman <joshfeldman500@gmail.com>
2023-05-04 10:52:43 -0400

    anagram done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+    # ["listen", "poison", "hello"]
+    #takes in a word_list that must be a list datatype and returns a list datatype
+    def match(self, word_list: list) -> list:
+    #declares empty matched_words array
+        matched_words = []
+    # for each element in the inputted word_list array, add it to the matched_words array if
+    # the sorted version of it matches the sorted version of the initially inputted self.word
+        for word in word_list:
+            matched_words.append(word) if sorted(word) == sorted(self.word) else None
+        return matched_words
